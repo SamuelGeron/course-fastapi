5f3e397838d1ce64f3c9e0565087a4887df2a98e
SamuelGeron <samuel_geron@hotmail.com>
2022-02-01 23:35:14 -0300

    pytest random tests added

---
+++ tests/test_calculations.py
@@ -0,0 +1,62 @@
+# Pytest Naming convetion file name: test_*.py, function test_*()
+from tracemalloc import start
+import pytest
+from app.calculations import add, subtract, multiply, divide, BankAccount, InsufficientFunds
+
+# Fixtures allways at the top
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+
+@pytest.mark.parametrize("num1, num2, expected", [
+    (3, 2, 5), (7, 1, 8), (12, 4, 16)
+])
+def test_add(num1, num2, expected):
+    assert add(num1, num2) == expected
+
+def test_subtract():
+    assert subtract(1,3) == -2
+
+def test_multiply():
+    assert multiply(1,3) == 3
+
+def test_divide():
+    assert divide(4,2) == 2
+
+
+def test_bank_set_initial_amount(bank_account):
+    assert bank_account.balance == 50
+
+def test_bank_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0
+
+def test_withdraw(bank_account):
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+
+def test_deposit(bank_account):
+    bank_account.deposit(30)
+    assert bank_account.balance == 80
+
+def test_collect_interest(bank_account):
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 6) == 55
+
+# Combining parameters and fixtures
+@pytest.mark.parametrize("deposited, withdrew, expected", [
+    (200, 40, 160), (50, 10, 40), (100, 70, 30)
+])
+def test_bank_transaction(zero_bank_account, deposited, withdrew, expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdrew)
+    assert zero_bank_account.balance == expected
+
+def test_insufficient_funds(bank_account):
+    with pytest.raises(InsufficientFunds):
+        bank_account.withdraw(200)
+
