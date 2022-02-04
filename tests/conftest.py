e254682c49660b515784a933b559ee92ff43da27
SamuelGeron <samuel_geron@hotmail.com>
2022-02-03 22:16:19 -0300

    added git hub action

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,6 +46,15 @@ def client(session):
 
 # USER
 @pytest.fixture
+def test_user2(client):
+    user_data = {"email": "test22@gmail.com", "password": "pass123"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+@pytest.fixture
 def test_user(client):
     user_data = {"email": "test2@gmail.com", "password": "pass123"}
     res = client.post("/users/", json=user_data)
@@ -68,7 +77,7 @@ def authorized_client(client, token):
 
 # POSTS
 @pytest.fixture
-def test_posts(test_user, session):
+def test_posts(test_user, test_user2, session):
     posts_data = [{
         "title": "first title",
         "content": "first content",
@@ -81,6 +90,10 @@ def test_posts(test_user, session):
         "title": "3rd title",
         "content": "3rd content",
         "owner_id": test_user['id']
+    },{
+        "title": "4rd title",
+        "content": "4rd content",
+        "owner_id": test_user2['id']
     }]
 
     def create_post_model(post):
--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,5 +1,6 @@
 from cgi import test
 from typing import List
+from venv import create
 import pytest
 from app import schemas
 from app.oauth2 import create_access_token
@@ -54,3 +55,59 @@ def test_create_post(authorized_client, test_user, test_posts, title, content, p
     assert created_post.published == published
     assert created_post.owner_id == test_user['id']
 
+
+def test_create_post_default_published_true(authorized_client, test_user, test_posts):
+    res = authorized_client.post("/posts/", json={"title": "title", "content": "content"})
+    created_post = schemas.Post(**res.json())
+    assert created_post.title == "title"
+    assert created_post.content == "content"
+    assert created_post.published == True
+    assert created_post.owner_id == test_user['id']
+
+
+def test_unauthorized_user_create_post(client, test_user, test_posts):
+    res = client.post("/posts/", json={"title": "title", "content": "content"})
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_delete_post(client, test_user, test_posts):
+    res = client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_delete_post_success(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 204
+
+
+def test_delete_post_non_exist(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(f"/posts/165168468416156")
+    assert res.status_code == 404
+
+
+def test_delete_other_user_post(authorized_client, test_user, test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[3].id}")
+    assert res.status_code == 403
+
+
+def test_update_post(authorized_client, test_user, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
+    updated_post = schemas.Post(**res.json())
+    assert res.status_code == 200
+    assert updated_post.title == data['title']
+    assert updated_post.content == data['content']
+
+
+def test_update_other_user_post(authorized_client, test_user, test_user2, test_posts):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[3].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[3].id}", json=data)
+    assert res.status_code == 403
